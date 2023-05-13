1424a38a73daa0fd979a27933a7e1d9118f77cdc
Ericles S <ericles2206@live.com>
2023-05-14 00:32:31 +1000

    Add date

--- Files/Models/FormElements.cs
+++ Files/Models/FormElements.cs
@@ -20,5 +20,6 @@ namespace Files.Models
         public String? Type { get; set; }
         public String? Email { get; set; }
         public String? Name { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
